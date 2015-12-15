b49015614813a13c341848b76ae675a8175fb004
ngyuki <ngyuki.jp@gmail.com>
2015-12-15 22:47:52 +0900

    remove packer

--- scripts/virtualbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# install virtualbox additions
-mount -o loop VBoxGuestAdditions_$(cat .vbox_version).iso /mnt
-/mnt/VBoxLinuxAdditions.run &&:
-
-# cleanup
-umount /mnt
-rm -vf VBoxGuestAdditions_*.iso
